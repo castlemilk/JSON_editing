133f3917d1a172ee57c1ca472d85f89ca67afc62
Ben Ebsworth <ben.ebsworth@gmail.com>
2016-12-29 16:58:41 +1100

    connect script extended

--- connect.sh
+++ connect.sh
@@ -4,21 +4,21 @@
 # food records for editing.
 
 
-USER = ${USER:-"editor"}
-PASSWD = ${PASSWD}
-DEST = ${DEST}
-HOSTNAME = ${HOSTNAME:-"editor"}
-KEY_DEST = ${KEY_DEST:-"~/.ssh/id_rsa"}
+USER=${USER:-editor}
+PASSWD=${PASSWD}
+RDEST=${RDEST:-"128.199.230.194"}
+RHOST=${RHOST:-editor}
+KEY_DEST=${KEY_DEST:-~/.ssh/id_rsa}
 
 function conf_hosts()
 {
-	echo "${DEST}	${HOSTNAME}" >> /etc/hosts
+	echo "${RDEST}	${RHOST}" >> /etc/hosts
 }
 
 function conf_SSH()
 {
-	echo "Host ${HOSTNAME}
-	Hostname ${HOSTNAME}
+	echo "Host ${RHOST}
+	Hostname ${RHOST}
 	User ${USER}
 	IdentityFile ${KEY_DEST}.pub" >> ~/.ssh/config	
 }
@@ -29,26 +29,41 @@ function create_key()
 }
 function copy_ssh_id()
 {
-	ssh-copy-id ${HOSTNAME}
+	ssh-copy-id ${RHOST}
 }
 
 function connect()
 {
-	ssh -N -L 27017:localhost:27017 ${HOSTNAME} &	
+	ssh -N -L 27017:localhost:27017 ${RHOST} &	
 	echo "Connection should be established. Now try"
 	echo " connect on localhost:27017 in Robomongo"
 }
 
 
-if ! grep -Fqx ".*${HOSTNAME}" /etc/hosts; then
-	conf_host
+if ! grep -Fqx "^.*${RHOST}" /etc/hosts; then
+	echo "adding configuration to host file"
+	conf_hosts
 fi
-if [ -v ${KEY_DEST} ]; then
+if [ ! -f ${KEY_DEST} ]; then
+	echo "creating ssh key"
 	create_key
 fi
-if ! grep -Fqx "Host ${HOSTNAME}" ~/.ssh/config; then
+if ! grep -Fqx "Host ${RHOST}" ~/.ssh/config; then
+	echo "configuring SSH"
 	conf_SSH
+	echo "copying SSH key to remote host"
 	copy_ssh_id
 fi
 
+current_connection_process=`ps -ax | grep ssh | grep '27017:localhost' | awk '{print $1}'`
+if [ -n "$current_connection_process" ]; then
+	for process in $current_connection_process; do
+		echo "found running tunnel with PID $process"
+		echo "killing now"
+		kill -9 $process
+		echo "done"
+	done
+else
+	connect
+fi
 
